b9958f8c84eb1c20766527b3978f5dfdaf246e7e
Néstor Martínez <nestorandres04@gmail.com>
2025-09-21 01:14:43 +0000

    lab finished

--- HourlyEmployee.cpp
+++ HourlyEmployee.cpp
@@ -1,9 +1,23 @@
 #include "HourlyEmployee.h"
 
-HourlyEmployee::HourlyEmployee(const std::string& name, int id, 
-                                double payPerHour, double workedHours)
-{
-    // TODO: 
+HourlyEmployee::HourlyEmployee(std::string name, int id, double payPerHour, double workedHours)
+    : Employee(name, id, "Hourly"), payPerHour(payPerHour), workedHours(workedHours) {
+    // Constructor calls parent Employee constructor with "Hourly" type
+}
+
+// Calculate bi-weekly pay (pay per hour * worked hours)
+double HourlyEmployee::calculatePay() const {
+    return payPerHour * workedHours;
+}
+
+// Getter for pay per hour
+double HourlyEmployee::getPayPerHour() const {
+    return payPerHour;
+}
+
+// Getter for worked hours
+double HourlyEmployee::getWorkedHours() const {
+    return workedHours;
 }
 /*TODO: calculate Payment */
 
